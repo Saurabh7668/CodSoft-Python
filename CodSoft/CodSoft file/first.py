76c38f173989d0f0c78a3d6b989b71af6777262e
Saurabh <chauhansaurabhbalwa@gmail.com>
2023-10-15 23:29:26 +0530

    First Code

---
+++ CodSoft/CodSoft file/first.py
@@ -0,0 +1,19 @@
+print('-------HELLO------')
+
+a=int(input("Enter the first numbher...."))
+b=int(input("Enter the secind number...."))
+print('Add two number.....')
+c=a+b
+print(c)
+print('Sub two number.....')
+d =a-b
+print(d)
+print('Multi two number.....')
+e=a*b
+print(e)
+print('Divide two number.....')
+f=a/b
+print(f)
+print("\n")
+print("Thank you....")
+print("Good Bay")
